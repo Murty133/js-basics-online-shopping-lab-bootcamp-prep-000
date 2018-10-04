2c3dc56951266250f2c8a4aebb8800ad9822b153
spoonnman <spoonnman@hotmail.com>
2018-10-04 03:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var string
-  if (cardNumber === null || cardNumber === "") {
+  if (!cardNumber) {
     string = "Sorry, we don't have a credit card on file for you."
   }
   else {
